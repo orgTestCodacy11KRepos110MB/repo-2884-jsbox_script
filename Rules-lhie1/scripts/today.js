6cc143a32e5141c1ed6d171d52d4589c5ae1b3d6
Fndroid <18825176954@163.com>
2018-05-12 06:57:35 +0800

    调整widget样式

--- Rules-lhie1/scripts/today.js
+++ Rules-lhie1/scripts/today.js
@@ -11,20 +11,23 @@ function renderTodayUI() {
     })
     $ui.render({
         props: {
+            id: "todayMainView",
             title: "Surge3规则生成",
-            frame: $rect(0, 0, sw, 110)
+            frame: $rect(0, 0, sw, 110),
+            hideNavbar: true,
+            // bgcolor: $rgba(255, 255, 255, 0)
         },
         views: [{
             type: "view",
             props: {
                 id: "",
-                // borderColor: $color("black"),
+                // borderColor: $rgba(50, 50, 50, 0.1),
                 // borderWidth: 2,
                 // radius: 10
             },
             layout: (make, view) => {
                 make.height.equalTo(110)
-                make.width.equalTo(view.super)
+                make.width.equalTo(view.super).offset(0)
                 make.center.equalTo(view.super)
             },
             views: [{
@@ -54,28 +57,32 @@ function renderTodayUI() {
             }, {
                 type: "image",
                 props: {
+                    id: "pullBtn",
                     data: $file.read("assets/today_pull.png"),
-                    radius: 25
+                    radius: 25,
+                    bgcolor: $rgba(255, 255, 255, 0)
                 },
                 layout: (make, view) => {
-                    make.width.height.equalTo(50)
-                    make.centerY.equalTo(view.super).offset(0)
+                    make.width.height.equalTo(55)
+                    make.centerY.equalTo(view.super).offset(-10)
                     make.centerX.equalTo(view.super)
                 },
                 events: {
                     tapped: sender => {
                         $app.openURL("jsbox://run?name=Rules-lhie1&auto=1")
                     }
-                }
+                },
             }, {
                 type: "image",
                 props: {
+                    id: "surgeBtn",
                     data: $file.read("assets/today_surge.png"),
-                    radius: 25
+                    radius: 25,
+                    bgcolor: $rgba(255, 255, 255, 0)
                 },
                 layout: (make, view) => {
-                    make.width.height.equalTo(50)
-                    make.centerY.equalTo(view.super).offset(0)
+                    make.width.height.equalTo(55)
+                    make.centerY.equalTo(view.super).offset(-10)
                     make.right.equalTo(view.prev.left).offset(- (sw / 9))
                 },
                 events: {
@@ -86,18 +93,62 @@ function renderTodayUI() {
             }, {
                 type: "image",
                 props: {
+                    id: "jsboxBtn",
                     data: $file.read("assets/today_jsbox.png"),
-                    radius: 25
+                    radius: 25,
+                    bgcolor: $rgba(255, 255, 255, 0)
                 },
                 layout: (make, view) => {
                     make.width.height.equalTo(50)
-                    make.centerY.equalTo(view.super).offset(0)
+                    make.centerY.equalTo(view.super).offset(-10)
                     make.left.equalTo(view.prev.prev.right).offset((sw / 9))
                 },
                 events: {
                     tapped: sender => {
                         $app.openURL("jsbox://run?name=Rules-lhie1")
                     }
+                }
+            }, {
+                type: "label",
+                props: {
+                    text: "更新规则",
+                    font: $font(12),
+                    textColor: $rgba(50, 50, 50, .8),
+                    align: $align.center
+                },
+                layout: (make, view) => {
+                    make.height.equalTo(10)
+                    make.top.equalTo($("pullBtn").bottom)
+                    make.width.equalTo($("pullBtn").width)
+                    make.centerX.equalTo($("pullBtn"))
+                }
+            }, {
+                type: "label",
+                props: {
+                    text: "Surge开关",
+                    font: $font(12),
+                    textColor: $rgba(50, 50, 50, .8),
+                    align: $align.center
+                },
+                layout: (make, view) => {
+                    make.height.equalTo(10)
+                    make.top.equalTo(view.prev.top)
+                    make.width.equalTo($("pullBtn").width)
+                    make.centerX.equalTo($("surgeBtn"))
+                }
+            }, {
+                type: "label",
+                props: {
+                    text: "脚本设置",
+                    font: $font(12),
+                    textColor: $rgba(50, 50, 50, .8),
+                    align: $align.center
+                },
+                layout: (make, view) => {
+                    make.height.equalTo(10)
+                    make.top.equalTo($("pullBtn").bottom)
+                    make.width.equalTo($("pullBtn").width)
+                    make.centerX.equalTo($("jsboxBtn"))
                 }
             }]
         }]
