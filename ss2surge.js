942f6b2efc6fce7ec5932b1bfeacbe54b36432d0
Fndroid <18825176954@163.com>
2018-05-26 10:30:11 +0800

    修复obfs识别

--- ss2surge.js
+++ ss2surge.js
@@ -7,6 +7,7 @@ function decodeScheme(url) {
             url += '#Unnamed'
         }
         tag = $text.URLDecode(url.match(/#(.*?)$/)[1])
+        console.log(url.match(/#(.*?)$/))
         if (url.includes('?')) {
             // tag = $text.URLDecode(url.match(/#(.*?)$/)[1])
             let mdps = url.match(/ss:\/\/(.*?)@/)[1]
@@ -22,7 +23,7 @@ function decodeScheme(url) {
             port = htpr.split(':')[1]
             let ps = $text.URLDecode(url.match(/\?(.*?)#/)[1])
             let obfs = ps.match(/obfs=(.*?);/)[1]
-            let obfsHost = ps.match(/obfs-host=(.*?)$/)[1]
+            let obfsHost = ps.match(/obfs-host=(.*?)[;$]{1}/)[1]
             plugin = `obfs=${obfs}, obfs-host=${obfsHost}`
         } else {
             let mdps = url.match(/ss:\/\/(.*?)#/)[1]
@@ -37,7 +38,7 @@ function decodeScheme(url) {
             proxy += `, ${plugin}`
         }
         return proxy
-    } catch {
+    } catch(e) {
         return null
     }
 }
@@ -65,7 +66,7 @@ function showResult(result) {
 }
 
 $ui.menu({
-    items: ['剪贴板读取链接', '扫描二维码', '相册选取二维码'],
+    items: ['剪贴板读取链接', '扫描二维码'],
     handler: function(title, idx) {
         if (idx == 0) {
             let result = decodeScheme($clipboard.text)
@@ -73,18 +74,9 @@ $ui.menu({
         } else if (idx == 1) {
             $qrcode.scan({
                 handler(string) {
+                    console.log(string)
                     let result = decodeScheme(string)
                     showResult(result)
-                }
-            })
-        }else {
-            $photo.pick({
-                format: "image",
-                handler: function(resp) {
-                    var image = resp.image
-                    let result = decodeScheme($qrcode.decode(image))
-                    showResult(result)
-                    
                 }
             })
         }
