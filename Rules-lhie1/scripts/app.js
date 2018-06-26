ce1687d7c7639ea85df558c2941f724db5d2c3b5
Fndroid <18825176954@163.com>
2018-06-26 19:08:06 +0800

    rename换行符问题

--- Rules-lhie1/scripts/app.js
+++ Rules-lhie1/scripts/app.js
@@ -1814,11 +1814,13 @@ function makeConf(params) {
                 })
                 renamePat.forEach(i => {
                     let oldName = i[0]
-                    let newName = i[1].replace(/\\=/g, '=')
+                    let newName = i[1].replace(/\\n/g, '\n').replace(/\\r/g, '\r').replace(/\\=/g, '=')
                     let oldNameReg = new RegExp(oldName, 'g')
                     prototype = prototype.replace(oldNameReg, newName)
                 })
             }
+
+            console.log('aaa\n\n' + prototype)
 
             let fn = (workspace.fileName || 'lhie1') + '.conf'
 
