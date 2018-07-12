95aeabfca227221052826c1fd4a315e30873193e
Fndroid <18825176954@163.com>
2018-07-12 11:18:30 +0800

    构建主界面

--- Searcher/scripts/app.js
+++ Searcher/scripts/app.js
@@ -1,7 +1,30 @@
-function sayHello() {
-  $ui.alert($l10n('HELLO_WORLD'));
+function renderMainUI() {
+  $ui.render({
+    props: {
+      title: "搜索"
+    },
+    views: [{
+      type: "view",
+      props: {
+        id: "mainView"
+      },
+      layout: $layout.fill,
+      views: [{
+        type: "input",
+        props: {
+
+        },
+        layout: (make, view) => {
+          make.height.equalTo(50);
+          make.width.equalTo(view.super).offset(-40);
+          make.centerY.equalTo(view.super).offset(-60);
+          make.centerX.equalTo(view.super);
+        }
+      }]
+    }]
+  })
 }
 
 module.exports = {
-  sayHello: sayHello
+  renderMainUI: renderMainUI
 }
