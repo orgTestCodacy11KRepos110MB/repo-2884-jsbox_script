6bc71edb6bda104aef63b714fdb56f7ac440bbe6
Fndroid <18825176954@163.com>
2018-08-17 00:14:13 +0800

    UI调整

--- QuantumultNoFA.js
+++ QuantumultNoFA.js
@@ -19,19 +19,47 @@ $app.autoKeyboardEnabled = true
 
 const CONF = 'https://raw.githubusercontent.com/lhie1/Rules/master/Quantumult/Quantumult.conf'
 
+const CACHEKEY = 'listData'
+
 function urlsaveBase64Encode(url) {
     return $text.base64Encode(url).replace(/\-/g, '+').replace(/\\/g, '_').replace(/=+$/, '')
 }
 
-const DEFAULT = '选择Google的Policy，不懂就不选 = PROXY\n选择微软服务的Policy，不懂就选择DIRECT = DIRECT\n选择PayPal的Policy，不懂就选择DIRECT = DIRECT\n选择Apple的Policy，不懂就选择DIRECT = DIRECT\n选择Netflix的Policy，不懂就不选 = PROXY\n,(?:PROXY|Proxy) = ,PROXY\n,DIRECT = ,DIRECT\n,REJECT = ,REJECT'
+const DEFAULT = [
+    {
+        reg: { text: "选择Google的Policy，不懂就不选" },
+        rep: { text: "PROXY" }
+    }, {
+        reg: { text: "选择微软服务的Policy，不懂就选择DIRECT" },
+        rep: { text: "DIRECT" }
+    }, {
+        reg: { text: "选择PayPal的Policy，不懂就选择DIRECT" },
+        rep: { text: "DIRECT" }
+    }, {
+        reg: { text: "选择Apple的Policy，不懂就选择DIRECT" },
+        rep: { text: "DIRECT" }
+    }, {
+        reg: { text: "选择Netflix的Policy，不懂就不选" },
+        rep: { text: "PROXY" }
+    }, {
+        reg: { text: "(?:PROXY|Proxy)" },
+        rep: { text: ",PROXY" }
+    }, {
+        reg: { text: ",DIRECT" },
+        rep: { text: ",DIRECT" }
+    }, {
+        reg: { text: ",REJECT" },
+        rep: { text: ",REJECT" }
+    }
+]
 
-let settings = $cache.get("settings")
-
-console.log('缓存', settings)
+let cacheData = $cache.get(CACHEKEY)
 
 $ui.render({
     props: {
-        title: "Quantumult"
+        title: "Quantumult",
+        navBarHidden: true,
+        statusBarStyle: 0
     },
     views: [{
         type: "view",
@@ -40,39 +68,89 @@ $ui.render({
         },
         layout: $layout.fill,
         views: [{
-            type: 'text',
+            type: 'list',
             props: {
-                bgcolor: $color("#f2f2f2"),
-                radius: 5,
-                text: settings? settings: DEFAULT,
-                id: 'inputView',
+                data: cacheData || DEFAULT,
+                rowHeight: 60,
+                id: "mainList",
+                actions: [{
+                    title: "delete",
+                    handler: (sender, indexPath) => {
+                        $cache.set(CACHEKEY, sender.data)
+                    }
+                }],
+                footer: {
+                    type: "view",
+                    props: {
+                        height: 65
+                    }
+                },
+                template: {
+                    props: {},
+                    views: [{
+                        type: "label",
+                        props: {
+                            id: "reg",
+                            font: $font("bold", 16)
+                        },
+                        layout: (make, view) => {
+                            make.height.equalTo(view.super.height).multipliedBy(0.5);
+                            make.width.equalTo(view.super).offset(-30);
+                            make.left.equalTo(view.super).offset(15);
+                            make.top.equalTo(view.super);
+                        }
+                    }, {
+                        type: "label",
+                        props: {
+                            id: "rep",
+                            font: $font("bold", 16),
+                            textColor: $color("#777")
+                        },
+                        layout: (make, view) => {
+                            make.height.equalTo(view.super.height).multipliedBy(0.5);
+                            make.width.equalTo(view.super).offset(-30);
+                            make.left.equalTo(view.super).offset(15);
+                            make.top.equalTo(view.prev.bottom)
+                        }
+                    }]
+                }
             },
             layout: (make, view) => {
-                make.height.equalTo(view.super).dividedBy(3)
-                make.width.equalTo(view.super).offset(-30)
+                make.height.equalTo(view.super).offset(-20)
+                make.width.equalTo(view.super)
                 make.centerX.equalTo(view.super)
-                make.top.equalTo(15)
+                make.top.equalTo(20)
             },
             events: {
-                didEndEditing: function(sender) {
-                    $cache.set('settings', sender.text)
+                didSelect: (sender, indexPath, data) => {
+                    let item = sender.object(indexPath)
+                    let newReg, newRep
+                    showAlterDialog(item.reg.text, item.rep.text, (newReg, newRep) => {
+                        console.log(newReg, newRep)
+                        let oldData = sender.data;
+                        oldData[indexPath.row] = {
+                            reg: { text: newReg },
+                            rep: { text: newRep }
+                        }
+                        sender.data = oldData
+                        $cache.set(CACHEKEY, sender.data)
+                    })
                 }
             }
         }, {
             type: 'button',
             props: {
-                title: '更新',
+                icon: $icon("165", $color("#fff"), $size(20, 20)),
                 id: 'genBtn',
+                radius: 25
             },
             layout: (make, view) => {
-                make.width.equalTo(view.prev)
-                make.centerX.equalTo(view.super)
-                make.height.equalTo(40)
-                make.top.equalTo(view.prev.bottom).offset(10)
+                make.height.width.equalTo(50)
+                make.bottom.right.equalTo(view.super).offset(-15)
             },
             events: {
                 tapped: async _ => {
-                    let pattern = $("inputView").text
+                    let pattern = $("mainList").data
                     try {
                         await startGen(pattern)
                     } catch (e) {
@@ -80,18 +158,160 @@ $ui.render({
                     }
                 }
             }
+        }, {
+            type: 'button',
+            props: {
+                icon: $icon("104", $color("#fff"), $size(20, 20)),
+                id: 'addBtn',
+                radius: 25
+            },
+            layout: (make, view) => {
+                make.height.width.equalTo(50)
+                make.bottom.equalTo(view.super).offset(-15)
+                make.right.equalTo(view.prev.left).offset(-15)
+            },
+            events: {
+                tapped: sender => {
+                    showAlterDialog('', '', (newReg, newRep) => {
+                        let oldData = $("mainList").data;
+                        oldData.push({
+                            reg: { text: newReg },
+                            rep: { text: newRep }
+                        })
+                        $("mainList").data = oldData;
+                        $cache.set(CACHEKEY, $("mainList").data)
+                    })
+                }
+            }
         }]
     }]
 })
+
+function showAlterDialog(reg, rep, callback) {
+    let view = {
+        type: "blur",
+        layout: $layout.fill,
+        props: {
+            id: "alertBody",
+            style: 1,
+            alpha: 0
+        },
+        views: [{
+            type: "view",
+            props: {
+                id: "alterMainView",
+                bgcolor: $color("#ccc"),
+                smoothRadius: 10
+            },
+            layout: (make, view) => {
+                make.height.equalTo(250);
+                make.width.equalTo(view.super).offset(-60);
+                make.center.equalTo(view.super)
+            },
+            events: {
+                tapped: sender => { }
+            },
+            views: [{
+                type: "label",
+                props: {
+                    text: "正则表达式",
+                    font: $font("bold", 16)
+                },
+                layout: (make, view) => {
+                    make.top.equalTo(view.super).offset(20);
+                    make.left.equalTo(view.super).offset(10);
+                }
+            }, {
+                type: "input",
+                props: {
+                    id: "alterInputReg",
+                    text: reg,
+                    autoFontSize: true
+                },
+                events: {
+                    returned: sender => {
+                        sender.blur()
+                    }
+                },
+                layout: (make, view) => {
+                    make.top.equalTo(view.prev.bottom).offset(10);
+                    make.width.equalTo(view.super).offset(-20);
+                    make.centerX.equalTo(view.super)
+                    make.left.equalTo(view.super).offset(10);
+                    make.height.equalTo(40)
+                }
+            }, {
+                type: "label",
+                props: {
+                    text: "替换文本",
+                    font: $font("bold", 16)
+                },
+                layout: (make, view) => {
+                    make.top.equalTo(view.prev.bottom).offset(15);
+                    make.left.equalTo(view.super).offset(10);
+                }
+            }, {
+                type: "input",
+                props: {
+                    id: "alberInputRep",
+                    text: rep,
+                    autoFontSize: true
+                },
+                events: {
+                    returned: sender => {
+                        sender.blur()
+                    }
+                },
+                layout: (make, view) => {
+                    make.top.equalTo(view.prev.bottom).offset(10);
+                    make.width.equalTo(view.super).offset(-20);
+                    make.centerX.equalTo(view.super)
+                    make.left.equalTo(view.super).offset(10);
+                    make.height.equalTo(40)
+                }
+            }, {
+                type: 'button',
+                props: {
+                    icon: $icon("064", $color("#fff"), $size(20, 20)),
+                    id: 'confirmBtn',
+                    radius: 25
+                },
+                layout: (make, view) => {
+                    make.height.width.equalTo(50)
+                    make.bottom.equalTo(view.super).offset(-15)
+                    make.right.equalTo(view.super).offset(-15)
+                },
+                events: {
+                    tapped: sender => {
+                        callback && callback($("alterInputReg").text, $("alberInputRep").text);
+                        $("alertBody").remove();
+                    }
+                }
+            }]
+        }],
+        events: {
+            tapped: sender => {
+                sender.remove()
+            }
+        }
+    }
+    $("mainView").add(view)
+    $ui.animate({
+        duration: 0.2,
+        animation: () => {
+            $("alertBody").alpha = 1
+        }
+    })
+}
 
 async function startGen(pattern) {
     let resp = await $http.get(CONF)
     let data = resp.data
 
-    let patterns = pattern.split(/[\n\r]+/g).map(i => {
-        return i.split(/\s*=\s*/)
-    }).filter(i => i.length === 2)
-    
+    let patterns = pattern.map(i => {
+        return [i.reg.text, i.rep.text]
+    })
+
     patterns.forEach(i => {
         data = data.replace(new RegExp(i[0], 'g'), i[1])
     })
@@ -103,16 +323,14 @@ async function startGen(pattern) {
         path: "Fndroid.conf"
     })
     if (success) {
-        $http.startServer({
-            handler: async function (result) {
-                let url = result.url + 'download?path=Fndroid.conf'
-                let test = await $http.get(url)
-                if (test.data) {
-                    let scheme = 'quantumult://configuration?filter=' + urlsaveBase64Encode(url)
-                    console.log('go')
-                    $app.openURL(scheme)
-                }
-            }
-        })
+        let result = await $http.startServer({})
+        console.log(result)
+        let url = result.url + 'download?path=Fndroid.conf'
+        let test = await $http.get(url)
+        const REJECT = 'https://raw.githubusercontent.com/lhie1/Rules/master/Quantumult/Quantumult_URL.conf'
+        if (test.data) {
+            let scheme = `quantumult://configuration?filter=${urlsaveBase64Encode(url)}&rejection=${urlsaveBase64Encode(REJECT)}`
+            $app.openURL(scheme)
+        }
     }
 }
