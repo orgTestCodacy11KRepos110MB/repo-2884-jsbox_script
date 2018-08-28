202aa40cff8d70681ee9101c04fbe0b719069ea7
Fndroid <18825176954@163.com>
2018-08-28 18:13:32 +0800

    修复加载慢或无法加载的问题

--- Rules-lhie1/main.js
+++ Rules-lhie1/main.js
@@ -1,7 +1,5 @@
 let socketLogger = require("socketLogger")
-'init' in socketLogger && socketLogger.init('192.168.50.229')
-
-console.log('设备信息', $device.info)
+'init' in socketLogger && socketLogger.init('192.168.50.229', 44555, false)
 
 const app = require('scripts/app')
 const init = require('scripts/init')
