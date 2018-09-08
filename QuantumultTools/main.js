f2dff1b8957aefc1a2bdcc59756b43bbb8a567e6
Fndroid <18825176954@163.com>
2018-09-08 16:48:51 +0800

    初始化

---
+++ QuantumultTools/main.js
@@ -0,0 +1,5 @@
+let socketLogger = require("socketLogger")
+typeof socketLogger.init === 'function' && socketLogger.init('127.0.0.1')
+// SocketLogger Auto Generation Code
+
+console.log($device.info)
