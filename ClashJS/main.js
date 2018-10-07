baeb314be3f34a6b50b4051fa8518feb3512a217
Fndroid <18825176954@163.com>
2018-10-07 23:11:10 +0800

    first box

--- ClashJS/main.js
+++ ClashJS/main.js
@@ -1,7 +1,3 @@
-let socketLogger = require("socketLogger")
-typeof socketLogger.init === 'function' && socketLogger.init('192.168.50.229')
-// SocketLogger Auto Generation Code
-
 console.clear()
 
 const _indexView = require('scripts/indexView')
